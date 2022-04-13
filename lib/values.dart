84812d3c1ed0b9e3dd50184145cdab9133b6d951
Ali Solanki <alisolanki123@gmail.com>
2022-04-13 13:22:18 +0530

    courses button added

--- lib/values.dart
+++ lib/values.dart
@@ -2,17 +2,19 @@ class Values {
   static const String name = "Ali Solanki";
   static const String description = "Lad with a million dreams 💎";
 
-  static const String floatingActionButtonName = "Consultation Booking";
+  static const String floatingActionButtonName = "Courses";
   static const String floatingActionButtonLink =
-      "https://appopener.com/web/pw5mqd3d1";
+      "https://courses.alisolanki.com";
   static const bool showFloatingActionButton = true; //true or false
 
   static const Map<String, String> links = {
+    'Courses 📀': "https://courses.alisolanki.com",
     'YouTube 🔴': "https://appopener.com/yt/jouwp3g7f",
     'Instagram 🦄': "https://appopener.com/ig/45rsyczsn",
     'Twitter 🔵': "https://appopener.com/web/rn3ju4s3y",
     'Consultation Booking (Crypto) 🗣': "https://appopener.com/web/pw5mqd3d1",
     'Student Samaj Community (Telegram) 🌐': "https://t.me/studentsamaj",
+    'Student Samaj Community (Discord) 🌐': "https://discord.gg/MAgUqf7vFk",
     'Github 🖥': "https://github.com/alisolanki",
     'LinkedIn 💼': "https://appopener.com/web/7as4cuydv",
     'Snapchat 🎉': "https://appopener.com/web/nsbwfqpw3",
